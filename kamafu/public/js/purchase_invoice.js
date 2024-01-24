8777d99e9d9c471719952a764a2535b709fb8447
lasalesi <larsericmueller@gmail.com>
2024-01-24 15:43:01 +0100

    added script to force load tax rate in items (override fetch from)

--- kamafu/public/js/purchase_invoice.js
+++ kamafu/public/js/purchase_invoice.js
@@ -13,11 +13,43 @@ frappe.ui.form.on('Purchase Invoice', {
 });
 
 frappe.ui.form.on('Purchase Invoice Item', {
+    rate: function(frm, cdt, cdn) {
+        update_tax(frm, cdt, cdn);
+    },    
     base_net_amount: function(frm, cdt, cdn) {
         update_tax(frm, cdt, cdn);
     },
     tax_rate: function(frm, cdt, cdn) {
         update_tax(frm, cdt, cdn);
-    }
+    },
+	item_code: function(frm, cdt, cdn) {
+        // this is to force loading the tax rate because the core fetch is very unreliable
+		var row = locals[cdt][cdn];
+		if (row.item_code) {
+		    frappe.call({
+                "method": "frappe.client.get",
+                "args": {
+                    "doctype": "Item",
+                    "name": row.item_code
+                },
+                "callback": function(response) {
+                    var item = response.message;
+                    if ((item.item_defaults.length > 0) && (item.item_defaults[0].expense_account)) {
+                        frappe.call({
+                            "method": "frappe.client.get",
+                            "args": {
+                                "doctype": "Account",
+                                "name": item.item_defaults[0].expense_account
+                            },
+                            "callback": function(response) {
+                                var account = response.message;
+                                frappe.model.set_value(cdt, cdn, "tax_rate", account.steuersatz);
+                            }
+                        });
+                    } 
+                }
+            });
+		}
+	}
 });
 
