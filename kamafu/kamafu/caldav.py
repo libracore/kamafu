9c9f0008372ff6f57ca16a2b2711828d2a8eae08
lasalesi <larsericmueller@gmail.com>
2024-02-23 21:49:03 +0100

    caldav feed

--- kamafu/kamafu/caldav.py
+++ kamafu/kamafu/caldav.py
@@ -34,22 +34,40 @@ def get_calendar(secret, user):
             AND (`stage` = "Gewonnen" OR `stage` LIKE "Offerte%");
     """, as_dict=True)
     # add events
-    for erp_visit in visits:
+    for erp_event in events:
         event = Event()
-        event.add('summary', erp_event['name'])
-        event.add('dtstart', erp_event['visit_date'])
+        event.add('summary', erp_event['event_name'])
+        event.add('dtstart', erp_event['event_date'])
         #if erp_event['ends_on']:
         #    event.add('dtend', erp_event['ends_on'])
         event.add('dtstamp', erp_event['modified'])
         event.add('description', "{0}\n\r{1}\n\r{2}".format(
-            erp_event['object_name'], 
-            erp_event['object_street'], 
-            erp_event['object_location']))
+            erp_event['event_name'], 
+            erp_event['lead_name'], 
+            erp_event['email_id']))
         # add to calendar
         cal.add_component(event)
     
     # get todos
-    
+    todos = frappe.db.sql("""
+        SELECT * 
+        FROM `tabToDo` 
+        WHERE 
+            `date` >= CURDATE()
+            AND `owner` = "{user}"
+            AND `status` = "Open";
+    """.format(user=user), as_dict=True)
+    # add todos
+    for erp_todo in todos:
+        todo = Todo()
+        todo.add('uid', erp_todo['name'])
+        todo.add('summary', erp_todo['description'])
+        todo.add('description', erp_todo['description'])
+        todo.add('created', erp_todo['creation'])
+        todo.add('last-modified', erp_todo['modified'])
+        # add to calendar
+        cal.add_component(todo)
+        
     return cal
 
 @frappe.whitelist(allow_guest=True)
